4f39fa07c0a4db5e74790eb921567ae5de339404
sannanjadoon2007-netizen <sannanjadoon2007@gmail.com>
2026-06-28 16:35:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-
 document.addEventListener("DOMContentLoaded", () => {
+
     const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector(".nav-links");
 
-    if (!menuToggle || !navLinks) {
-        console.log("Menu elements not found");
-        return;
-    }
+    if (!menuToggle || !navLinks) return;
 
     menuToggle.addEventListener("click", () => {
         navLinks.classList.toggle("show");
     });
+
 });
-console.log("menu loaded");
