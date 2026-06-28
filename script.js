3b9e53f3c0c050215d58701a0a21274f40610958
sannanjadoon2007-netizen <sannanjadoon2007@gmail.com>
2026-06-28 16:17:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,9 @@ navItems.forEach(link => {
         navLinks.classList.remove("show");
     });
 });
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
